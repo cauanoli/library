644b9b941c9242ab55a2b8bbd076829eb5629eae
Cauan <cauandeoliveiracosta@gmail.com>
2024-05-05 17:04:05 -0300

    feat: add create modal functionality

--- main.js
+++ main.js
@@ -1,11 +1,4 @@
-const library = [
-  new Book({
-    title: "something",
-    pageNumber: 12321,
-    author: "s",
-    isRead: false,
-  }),
-];
+const library = [];
 
 function Book({ title, author, pageNumber, isRead }) {
   this.title = title;
@@ -18,6 +11,21 @@ function addBookToLibrary(bookInfo) {
   const book = new Book(bookInfo);
 
   library.push(book);
+  updateLibrary();
+}
+
+function updateLibrary() {
+  resetLibrary();
+  renderLibrary();
+}
+
+function resetLibrary() {
+  const booksTable = document.querySelector(".books-table");
+  const books = booksTable.querySelectorAll(".book");
+
+  books.forEach((book) => {
+    booksTable.removeChild(book);
+  });
 }
 
 function renderLibrary() {
@@ -43,16 +51,23 @@ function createBookElement(book) {
   pages.textContent = book.pageNumber;
   status.textContent = book.isRead ? "Read" : "Not Read";
 
-  bookElement.appendChild(title);
-  bookElement.appendChild(author);
-  bookElement.appendChild(pages);
-  bookElement.appendChild(status);
-
   return bookElement;
 }
 
 const addBookDialog = document.querySelector(".add-book-dialog");
 const addBookButton = document.querySelector(".add-book-button");
+
+const addBookForm = addBookDialog.querySelector(
+  ".add-book-dialog__form"
+);
+
+addBookForm.addEventListener("submit", (event) => {
+  console.log(event);
+  const formData = new FormData(event.target);
+  const bookData = Object.fromEntries(formData.entries());
+
+  addBookToLibrary(bookData);
+});
 
 addBookButton.addEventListener("click", () => {
   addBookDialog.showModal();
